33eec925ec7d9ce14e172dcd8f554ab87c396cc0
Ibra <ibrahima.dansokho@outlook.com>
2016-02-19 12:20:48 +0100

    add route

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import {bootstrap}    from 'angular2/platform/browser'
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppComponent} from './app.component'
 // import {enableProdMode} from 'angular2/core';
 //
 // enableProdMode();
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS
+]);
